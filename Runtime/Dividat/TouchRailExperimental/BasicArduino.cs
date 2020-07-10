968840558205e65389a040a22879b67c5ec1e16f
Stefan <stef@sutee.ch>
2020-07-10 19:05:27 +0200

    [feat] Experimental TouchRail Input
    * Runs based on Serial connection to Arduino. IMPORTANT: INCOMPATIBLE WITH WEBGL!!
    	* Multithreaded and non-blocking implementation
    * Visualizer for single handrail
    * Right Clicking on TouchRail Component reveals a ListPorts menu, which can be used to help find the correct port name for the arduino.

---
+++ Runtime/Dividat/TouchRailExperimental/BasicArduino.cs
@@ -0,0 +1,61 @@
+﻿using System.IO.Ports;
+using System.Threading;
+using System;
+
+namespace Dividat.TouchRail {
+    public class ArduinoConnection
+    {
+        string port;
+        SerialPort serial;
+
+        public string LastLine {
+            get {
+                return _lastLine;
+            }
+        }
+        private string _lastLine;
+        private bool _connected;
+        
+        public event ArduinoNotification OnLineReceived;
+        public delegate void ArduinoNotification();
+
+
+        //Main Thread Variables
+        private Thread readThread;
+
+
+        public ArduinoConnection(string serialPort, int baud=9200, bool autoConnect = true){
+            port = serialPort;
+            serial = new SerialPort(serialPort, baud);
+            serial.ReadTimeout = 500;
+            if (autoConnect){
+                Connect();
+            }
+        }
+
+        public void Connect(){
+            if (!serial.IsOpen)
+				serial.Open ();
+            _connected = true;
+            readThread = new Thread(ReadLine);
+            readThread.IsBackground = true;
+            readThread.Start();
+        }
+
+       private void ReadLine(){
+            while (_connected){
+                try {
+                    _lastLine = serial.ReadLine();
+                    OnLineReceived?.Invoke();
+                }
+                catch (TimeoutException) { }
+            }      
+        }
+
+        public void Close(){
+            _connected = false;
+            readThread.Join();
+            serial.Close();
+        }
+    }
+}
---
+++ Runtime/Dividat/TouchRailExperimental/TouchRail.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+
+namespace Dividat.TouchRail {
+    public class TouchRail : MonoBehaviour
+    {
+        [Header("Touch Rail Behaviour and Connection")]
+        public string comPort = "/dev/tty.usbmodem14101";
+
+        [Range(1f, 300f)]
+        public float thresholdTouched = 30f;
+
+        [Range(60f, 600f)]
+        public float thresholdPressed = 200f;
+
+        [Header("Component Setup")]
+        public GameObject[] touchZones;
+        
+        //Private Status Vars
+        private ArduinoConnection _arduino;
+        private int _lastUpdatedFrame = -1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ListPorts();
+            _arduino = new ArduinoConnection(comPort, 115200);
+            // _arduino.OnLineReceived+= UpdateTouchRailStatus;
+        }
+
+        // void UpdateTouchRailStatus(){
+        //     Debug.Log("Update in frame" + _lastUpdatedFrame);
+        // }
+
+        void Update(){
+            string railRaw = _arduino.LastLine;
+            if (railRaw != null && railRaw != ""){
+                string[] values = railRaw.Split(',');
+                if (values.Length != 8){
+                    Debug.Log(values.Length);
+                }
+                else {
+                    _lastUpdatedFrame = Time.frameCount;
+
+                    for (int i=2; i < values.Length; i++){
+                        float pressureValue = float.Parse(values[i]);
+                        if (pressureValue > thresholdPressed){
+                            touchZones[i-2].transform.localScale = new Vector3(0.20f, 0.75f, 0.20f);
+                            Debug.Log("active=" + i + " value= " + values[i]);
+                        }
+                        else if (pressureValue > thresholdTouched){
+                            touchZones[i-2].transform.localScale = new Vector3(
+                                0.4f - 0.2f*(pressureValue-thresholdTouched)/(thresholdPressed-thresholdTouched), 
+                                0.75f, 
+                                0.4f - 0.2f*(pressureValue-thresholdTouched)/(thresholdPressed-thresholdTouched)
+                            );
+                            Debug.Log("active=" + i + " value= " + values[i]);
+                        }
+                        else {
+                            touchZones[i-2].transform.localScale = new Vector3(0.4f, 0.75f, 0.4f);
+                        }
+                    }
+                }
+            }
+        }
+
+        [ContextMenu("List Ports")]
+        public void ListPorts(){
+            Debug.Log("Available Port Names");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Debug.Log(s);
+            }
+        }
+
+        public void OnDestroy(){
+            if (_arduino != null){
+                _arduino.Close();
+            }
+        }
+    }
+}
